e1746946821a59a69a3b46a53afbca01051cbf3d
Spurny <SpurnyTo@vda.cz>
2025-05-30 08:45:36 +0200

    program zapisuje jména pod sebe

--- BMI.cmd
+++ BMI.cmd
@@ -3,11 +3,12 @@
 REM nacitani promennych
 set /p vyska=zadej vysku v cm:
 set /p vaha=zadej vahu v kg:
+set /p jmeno=zadej sve jmeno:
 
 set /a vypocet= %vaha% * 10000 / (%vyska%*%vyska%)
 
-echo BMI pro vysku %vyska% a vahu %vaha% je: %vypocet% > bmi.txt
+echo pro klienta %jmeno% BMI pro vysku %vyska% a vahu %vaha% je: %vypocet% >> bmi.txt
 
-echo BMI pro vysku %vyska% a vahu %vaha% je: %vypocet%
+echo pro klienta %jmeno% BMI pro vysku %vyska% a vahu %vaha% je: %vypocet%
 
 pause
