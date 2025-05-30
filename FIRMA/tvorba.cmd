2ec1cfdd437d4a2b8f3adfeb4387d836f44c443c
Spurny <SpurnyTo@vda.cz>
2025-05-30 09:34:38 +0200

    finalni verze zakazky

--- FIRMA/tvorba.cmd
+++ FIRMA/tvorba.cmd
@@ -1,13 +1,18 @@
 @echo off
 
+REM vytovreni nazvu zakazky
 set /p zakazka=zadej jmeno zakazky
 
+REM vytvoreni slzky s danym jmenem
 mkdir %zakazka% 
 
 mkdir .\%zakazka%\%zakazka%_nabidka
 mkdir .\%zakazka%\%zakazka%_smlouva
 mkdir .\%zakazka%\%zakazka%_faktura
 
-
+REM kopirovani souboru
+copy .\smlouva.docx .\%zakazka%\%zakazka%_smlouva\%zakazka%_smlouva.docx
+copy .\nabidka.docx .\%zakazka%\%zakazka%_nabidka\%zakazka%_nabidka.docx
+copy .\faktura.docx .\%zakazka%\%zakazka%_faktura\%zakazka%_faktura.docx
 
 pause
